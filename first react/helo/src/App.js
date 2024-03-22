7223b63624af7d22c1154ec5725b6fdba47757e3
Tiruchowdary <thirumalathumati729@gmail.com>
2024-03-22 16:18:21 +0530

    hello

---
+++ first react/helo/src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <h1><center>RESUME</center></h1>
+    <br/>
+    <br/>
+    <h3 className='first'>THUMATI THIRUMALA</h3>
+    <h4 className='end3'>Emali:thirumalathumati729@gmail.com</h4><br/><br/>
+    <h4 className='end2'>phn no:9848090509</h4><br/><br/><br/><br/>
+    <hr/>
+    <h4 className='end'>Carrer Objective:</h4>
+    <p>• Seeking a position to utilize my skills and abilities in the Information Technology &
+       Innovative Organization that an offers professional growth & personal level while
+      being resourceful, innovative and flexible.</p>
+      <h4 className='end'>Education Details:</h4>
+      <section>
+        <ul>
+
+          <li>B.TECH [COMPUTER SCIENCE AND ENGINEERING]</li>
+        </ul>
+        </section>
+        <h4 className='end'>Techincal skills</h4>
+        <ul>
+          <li>  HTML</li>
+          <li>  CSS</li>
+          <li>  JAVASCRIPT</li>
+          <li>  SQL</li>
+          <li>  REACT</li>
+        </ul>
+        <h4 className='end'>Project:</h4>
+        <ul>
+          <li>Flipkart clone</li>
+          <li>Vote polling</li>
+        </ul>
+        <h4 className='end'>Area of Interest</h4>
+      <p>Travelling,cooking,Listening Music</p>
+      <h4 className="end">Personal Profile</h4>
+      <ul>
+        <li>Name : THUMATI THIRUMALA</li>
+        <li>Father Name : T VENKATESWARLU</li>
+        <li>Mother's Name : T BHAGYAMMA</li>
+        <li>Date of Birth : 15-09-2001</li>
+        <li>Language Known : English,Telugu,Hindi</li>
+      </ul>
+
+      <h4 className="end">Declaration</h4>
+      <p>“I hereby declare that all the details mentioned above are in accordance with the truth
+and fact as per my knowledge and I hold the responsibility for the correctness of
+the above-mentioned particulars.”
+<h5 className="end1">THUMATI THIRUMALA</h5>
+</p>
+
+
+
+     
+
+    
+     
+    </div>
+  );
+}
+
+export default App;
